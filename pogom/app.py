77e12aa355e347c1201874192453a482b9c19479
root <myntath@gmail.com>
2017-02-08 10:52:04 +1100

    strip out some auth stuff meant for other commits

--- pogom/app.py
+++ pogom/app.py
@@ -164,7 +164,6 @@ class Pogom(Flask):
                                show_scan=scan_display
                                )
 
-    @requires_auth
     def raw_data(self):
         self.heartbeat[0] = now()
         args = get_args()
@@ -388,7 +387,6 @@ class Pogom(Flask):
             log.info('Changing next location: %s,%s', lat, lon)
             return self.loc()
 
-    @requires_auth
     def list_pokemon(self):
         # todo: Check if client is Android/iOS/Desktop for geolink, currently
         # only supports Android.
@@ -514,7 +512,6 @@ class Pogom(Flask):
         valid_input["order"] = {"display": "Order", "items": valid_order}
         return valid_input
  
-    @requires_auth
     def get_stats(self):
         return render_template('statistics.html',
                                lat=self.current_location[0],
