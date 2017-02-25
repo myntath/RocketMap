18539c2ee3714793b071b2492b0733b406d6a58e
Alderon86 <christianbriest86@gmail.com>
2017-02-25 01:36:48 +0100

    dont mess up branches

--- pogom/search.py
+++ pogom/search.py
@@ -762,6 +762,7 @@ def search_worker_thread(args, account_queue, account_failures,
             status['success'] = 0
             status['noitems'] = 0
             status['skip'] = 0
+            status['captcha'] = 0
 
             stagger_thread(args)
 
